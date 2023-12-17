e130eff2d5e89965a71a164bd8f104238cdbe509
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-17 11:10:46 +0100

    parse application/json, application/x-www-urlencoded

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const errorHandler = require("./errorHandler");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use(notFound);
 app.use(errorHandler);
 
