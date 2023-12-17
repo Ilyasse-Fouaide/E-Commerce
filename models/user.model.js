816e8646c78019baa08116c2b3475942edf09ab9
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-17 12:04:18 +0100

    add user schema and user model

---
+++ models/user.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true
+  },
+}, { timestamps: true });
+
+const User = mongoose.model("users", userSchema);
+
+module.exports = User
