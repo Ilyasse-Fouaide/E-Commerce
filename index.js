efe976eac4936feb47a8f22de61b052871fe69c9
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-17 11:16:46 +0100

    add xss-clean instead of xss

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const config = require("./config");
 const connect = require("./db/connect");
 const notFound = require("./middlewares/notFound");
 const errorHandler = require("./errorHandler");
+// security packages
+const xss = require("xss-clean");
+const helmet = require("helmet");
+const cors = require("cors");
 
 const app = express();
 
+// middlewares
+app.use(cors({ origin: "*" }));
+app.use(xss());
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
