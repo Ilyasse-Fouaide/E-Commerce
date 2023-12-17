672694ae883ddd3785cac99676b93fc2c67a1d7f
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-17 10:59:06 +0100

    connect to the mongo database

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const config = require("./config");
+const connect = require("./db/connect");
+
 const app = express();
 
 const port = config.APP_PORT || 5000
 
-const start = () => {
+const start = async () => {
   try {
+    await connect();
     app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
   } catch (error) {
     throw new Error(error)
