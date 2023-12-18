d18f064b700d5b524ccf3ce73ab9c8384fb02ea1
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-18 20:25:43 +0100

    Changed the error type from unauthorized to forbidden.

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -1,9 +1,9 @@
 const { ReasonPhrases } = require("http-status-codes");
-const { unauthorizedError } = require('../customError');
+const { forbiddenError } = require('../customError');
 
 const isAdmin = (req, res, next) => {
   if (req.user.role !== "admin") {
-    return next(unauthorizedError(`${ReasonPhrases.UNAUTHORIZED}, only admin can access to this route.`));
+    return next(forbiddenError(`403 - ${ReasonPhrases.FORBIDDEN}`));
   }
   next()
 }
