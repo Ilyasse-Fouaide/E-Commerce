a0aacb67600fa3305a3f435355973bf1e79209e2
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-18 08:07:34 +0100

    handle duplicate email error

--- errorHandler/index.js
+++ errorHandler/index.js
@@ -5,19 +5,26 @@ const errorHandler = (err, req, res, next) => {
   let HttpStatusCode = err.HttpStatusCode || StatusCodes.INTERNAL_SERVER_ERROR;
   let error = {
     success: false,
-    message: {
+    error: {
       status: HttpStatusCode,
-      error: err.message || ReasonPhrases.INTERNAL_SERVER_ERROR
+      message: err || ReasonPhrases.INTERNAL_SERVER_ERROR
     }
   }
 
   if (err.name && err.name === "ValidationError") {
     HttpStatusCode = StatusCodes.BAD_REQUEST;
-    error.message.status = StatusCodes.BAD_REQUEST;
-    error.message.error = Object.values(err.errors).map(({ path, message }) => {
+    error.error.status = HttpStatusCode;
+    error.error.message = Object.values(err.errors).map(({ path, message }) => {
       return { [path]: message }
     });
   }
+
+  if (err.code && err.code === 11000) {
+    HttpStatusCode = StatusCodes.BAD_REQUEST;
+    error.error.status = HttpStatusCode;
+    error.error.message = `Email already taken.`;
+  }
+
 
   res.status(HttpStatusCode).json(error)
 }
