59ca6590716affaddc70c207218f4be1a649d23f
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-20 15:27:42 +0100

    Add product not found error handling in review creation.

--- controllers/review.controller.js
+++ controllers/review.controller.js
@@ -1,12 +1,34 @@
 const { StatusCodes } = require("http-status-codes");
 const tryCatchWrapper = require("../tryCatchWrapper");
 const Review = require("../models/review.model");
+const Product = require("../models/product.model");
+const { notFoundError } = require("../customError");
 
 module.exports.index = tryCatchWrapper(async (req, res, next) => {
+  const { rating, title, review, productId } = req.body;
+  const { userId } = req.user;
+
+  const product = await Product.findById(productId);
+
+  if (product) {
+    return next(notFoundError("no product found."))
+  }
+
+  await Review.create({
+    rating,
+    title,
+    review,
+    user: userId,
+    productId
+  });
+
   res.status(StatusCodes.OK).json({ success: true })
 });
 
 module.exports.store = tryCatchWrapper(async (req, res, next) => {
+
+
+
   res.status(StatusCodes.CREATED).json({ success: true })
 });
 
