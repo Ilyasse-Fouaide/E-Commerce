9180fecff11758ae920698a1624f6a118874c71a
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-21 09:43:48 +0100

    Refactor the code and Add virtuals property

--- models/product.model.js
+++ models/product.model.js
@@ -1,69 +1,76 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-    maxlength: [100, "Name cannot be more than 100 characters"]
-  },
-  price: {
-    type: Number,
-    required: true,
-    trim: true
-  },
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-    maxlength: [1000, "Description cannot be more than 1000 characters"]
-  },
-  image: {
-    type: String,
-    required: false,
-    default: "/upload/images/default.jpg"
-  },
-  category: {
-    type: String,
-    required: true,
-    enum: {
-      values: ['office', 'kitchen', 'bedroom'],
-      message: '{VALUE} is not supported'
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: [100, "Name cannot be more than 100 characters"]
+    },
+    price: {
+      type: Number,
+      required: true,
+      trim: true
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: [1000, "Description cannot be more than 1000 characters"]
+    },
+    image: {
+      type: String,
+      required: false,
+      default: "/upload/images/default.jpg"
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: {
+        values: ['office', 'kitchen', 'bedroom'],
+        message: '{VALUE} is not supported'
+      }
+    },
+    company: {
+      type: String,
+      required: true,
+      enum: {
+        values: ['ikea', 'liddy', 'marcos'],
+        message: '{VALUE} is not supported'
+      }
+    },
+    colors: [{
+      type: String,
+    }],
+    featured: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    freeShipping: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    inventory: Number,
+    averageRating: {
+      type: Number,
+      required: false,
+      default: 0
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "users",
+      required: true
     }
   },
-  company: {
-    type: String,
-    required: true,
-    enum: {
-      values: ['ikea', 'liddy', 'marcos'],
-      message: '{VALUE} is not supported'
-    }
-  },
-  colors: [{
-    type: String,
-  }],
-  featured: {
-    type: Boolean,
-    required: false,
-    default: false
-  },
-  freeShipping: {
-    type: Boolean,
-    required: false,
-    default: false
-  },
-  inventory: Number,
-  averageRating: {
-    type: Number,
-    required: false,
-    default: 0
-  },
-  user: {
-    type: mongoose.Types.ObjectId,
-    ref: "users",
-    required: true
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   }
-}, { timestamps: true });
+);
 
 const Product = mongoose.model("products", productSchema);
 
